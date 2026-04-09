8753bf4d49478b9198656d6851ac0b6aeeba7d47
ashwaniiikuma <Ashwaniiikumar1122@gmail.com>
2026-04-09 23:43:33 +0530

    Fix: Added missing MongoDBAdopter impor

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,6 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import { MongoDBAdapter } from "@auth/mongodb-adapter"
 
 // 1. authOptions ko alag se export karein
 export const authOptions = {

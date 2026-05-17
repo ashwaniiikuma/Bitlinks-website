5549846b4a3ccae5863d0c103770bf480c591875
ashwaniiikuma <Ashwaniiikumar1122@gmail.com>
2026-05-17 21:39:22 +0530

    fix: ui spacing

--- app/page.js
+++ app/page.js
@@ -31,16 +31,16 @@ export default function Home() {
   }, []);
   return (
     <main>
-      <section className="grid grid-cols-1 md:grid-cols-2 min-h-[70vh]">
+      <section className="grid grid-cols-1 md:grid-cols-2  min-h-[60vh]">
 
         {/* Left Content */}
-        <div className="flex flex-col gap-1 md:gap-2 items-center justify-center text-center px-6 md:px-10 py-20">
+        <div className="flex flex-col gap-1 md:gap-2 items-center justify-center text-center px-6 md:px-10 py-10 md:py-20">
           
           <p className={`text-2xl md:text-3xl font-bold py-5 ${poppins.className}`}>
             The best URL shortener in the market
           </p>
 
-          <p className="text-gray-700 max-w-2xl py-10">
+          <p className="text-gray-700 max-w-2xl py-8 md:py-10">
 We deliver a reliable URL shortening solution that enables users to create, monitor, and manage links effectively with a clean interface.
           </p>
 
@@ -59,7 +59,7 @@ We deliver a reliable URL shortening solution that enables users to create, moni
             </AnimatePresence>
           </div>
 
-          <div className="flex flex-col sm:flex-row gap-6 py-10">
+          <div className="flex flex-col sm:flex-row gap-6 py-8 md:py-10">
             <Link href="/shorten">
               <button className="bg-purple-500 px-6 py-2 text-white cursor-pointer rounded-lg font-bold hover:bg-purple-600 transition">
                 Try Now
